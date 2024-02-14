6d0953faa2877553a9bc21462f6feacfc2cbcdb9
Fahad Kiani <fjkiani@icloud.com>
2024-02-14 13:22:25 -0500

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">100% of your membership fees</span> donated directly to charity {" "}
+            <span className="text-white">30% of membership fees</span> donated to help rebuild Palestine {" "}
             {/* <span className="text-white">1 Month</span> Account */}
           </p>
         </div>
@@ -17,7 +17,7 @@ const Hero = () => {
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
             Empowering Investors <br className="sm:block hidden" />{" "}
-            <span className="text-gradient"> Sadaqah</span>{" "}
+            <span className="text-gradient">Stock Market</span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
